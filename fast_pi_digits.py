1c9b63cffb9124d38fd1943ddcbe291a5bc2888e
Barkerbg001 <barkerbg001@gmail.com>
2025-05-18 15:49:33 +0200

    Refactor Pi computation to use Chudnovsky algorithm and add requirements.txt for gmpy2

--- fast_pi_digits.py
+++ fast_pi_digits.py
@@ -1,23 +1,26 @@
-import decimal
-import math
+import gmpy2
+from gmpy2 import mpz, mpq, mpfr, get_context
 
-def compute_pi(digits):
-    decimal.getcontext().prec = digits + 2  # extra digits for accuracy
-    a = decimal.Decimal(1)
-    b = decimal.Decimal(1) / decimal.Decimal(2).sqrt()
-    t = decimal.Decimal(0.25)
-    p = decimal.Decimal(1)
-    for _ in range(int(math.log2(digits)) + 1):
-        an = (a + b) / 2
-        b = (a * b).sqrt()
-        t -= p * (a - an) ** 2
-        a = an
-        p *= 2
-    pi = (a + b) ** 2 / (4 * t)
-    return str(pi)[:digits + 2]  # 3. + digits
+def compute_pi_chudnovsky(digits):
+    get_context().precision = digits * 4  # increase to reduce error
+    C = 426880 * gmpy2.sqrt(10005)
+    M = mpz(1)
+    L = mpz(13591409)
+    X = mpz(1)
+    K = mpz(6)
+    S = mpz(L)
+
+    for i in range(1, digits // 14):  # each term adds ~14 digits
+        M = (K**3 - 16*K) * M // (i**3)
+        L += 545140134
+        X *= -262537412640768000
+        S += M * L // X
+        K += 12
+
+    pi = C / S
+    return str(pi)[:digits + 2]
 
 if __name__ == "__main__":
-    digits = int(input("Enter number of digits to calculate for Pi: "))
-    pi_digits = compute_pi(digits)
-    print(f"Pi to {digits} digits:")
-    print(pi_digits)
+    digits = int(input("How many digits of Pi? "))
+    print("Calculating...")
+    print(compute_pi_chudnovsky(digits))
